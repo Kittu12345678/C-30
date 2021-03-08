1901f1f5a7e57d11aa357182bc3de20532e5a74a
kittu12345678 <nihanth.gudala@gmail.com>
2021-03-08 18:48:04 +0530

    platform

--- sketch.js
+++ sketch.js
@@ -67,6 +67,12 @@ function draw(){
     slingshot.display();    
 }
 
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body);
+    }
+}
+
 function mouseDragged(){
     Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
 }
